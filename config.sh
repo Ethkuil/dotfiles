8c8d7e359aab9794668fc1ca625ed7c37fd04424
Ethkuil <hobartlin23@gmail.com>
2024-06-14 20:23:29 +0800

    feat: add my custom `.inputrc`

--- config.sh
+++ config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-./batch_relink.sh . ~ .bashrc .gitconfig .pythonrc .vimrc .ssh/
+./batch_relink.sh . ~ .bashrc .inputrc
+./batch_relink.sh . ~ .ssh/
+
+./batch_relink.sh . ~ .gitconfig
+./batch_relink.sh . ~ .pythonrc
+./batch_relink.sh . ~ .vimrc
 
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
