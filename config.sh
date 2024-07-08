9505d4de440f9a88780598d500ab93eb64b7e964
Ethkuil <hobartlin23@gmail.com>
2024-07-08 21:34:02 +0800

    refactor(config.sh)

--- config.sh
+++ config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-./batch_relink.sh . ~ .bashrc .bash_aliases .inputrc
+./batch_relink.sh . ~ .inputrc .tmux.conf
 ./batch_relink.sh . ~ .ssh/
 
-./batch_relink.sh . ~ .gitconfig
+./batch_relink.sh . ~ .bashrc .bash_aliases
+./batch_relink.sh . ~ .gitconfig .vimrc
+
 ./batch_relink.sh . ~ .pythonrc
-./batch_relink.sh . ~ .vimrc
 
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
